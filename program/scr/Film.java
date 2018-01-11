a7502acb77da28681bf4e2a68931f0fb55dc86d4
kimvg2 <kimvg2@hotmail.com>
2018-01-11 16:37:35 +0100

    Film.java, Films.java en FilmRepo.java aangemaakt
    Geen idee hoe ik nu verder moet...

---
+++ program/scr/Film.java
@@ -0,0 +1,18 @@
+public class Film {
+    private int id;
+    private String name;
+    private String language;
+    private String duration;
+    private String genre;
+    private int age;
+
+    public Film(int id, String name, String language, String duration, String genre, int age) {
+        this.id = id;
+        this.name = name;
+        this.language = language;
+        this.duration = duration;
+        this.genre = genre;
+        this.age = age;
+    }
+
+}
