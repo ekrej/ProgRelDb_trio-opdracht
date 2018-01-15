aa74cae6d397a32c5c3ef365af69033b87521e75
kimvg2 <kimvg2@hotmail.com>
2018-01-15 22:47:52 +0100

    Overview OnlyOne

--- program/scr/Overviews/OnlyOne.java
+++ program/scr/Overviews/OnlyOne.java
@@ -1,7 +1,25 @@
 package Overviews;
 
+
+import javax.swing.*;
 import java.awt.*;
 
 public class OnlyOne {
+    private Container container;
+    private String email;
+
+    public OnlyOne(Container container, String email) {
+        this.container = container;
+        this.email = email;
+    }
+
+    public void onlyOneLabels() {
+        //setting layout
+        GridLayout layout = new GridLayout(20, 1);
+        container.setLayout(layout);
+
+        JLabel accountLabel = new JLabel(email);
+        container.add(accountLabel);
+    }
 
 }
